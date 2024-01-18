dd8d7fea7629236e9afbaf68443f0464fafd8bdb
ArianaGrandeMeal <trippsellers22@gmail.com>
2024-01-18 13:55:51 -0500

    Updated main.py to reflect guided project code, added functional experimental grid drawing in multiline comment.

--- graphics.py
+++ graphics.py
@@ -1,7 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
-    
     def __init__(self, width, height):
         self.__root=Tk()
         self.__root.title("Maze Solver")
@@ -20,27 +19,26 @@ class Window:
             self.redraw()
         print("window closed...")
 
+    def draw_line(self, line, fill_color = "black"):
+        line.draw(self.__canvas, fill_color )
+
     def close(self):
         self.__running = False
 
-
-
+    
 class Point:
-
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
 class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
 
-    def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
-
-    def draw(self, Canvas, fill_color):
-        canvas = Canvas
-        self.fill_color = fill_color
-
-        canvas.create_line(x1, y1, x2, y2, )
-        canvas.pack()
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+        canvas.pack(fill=BOTH, expand=1)
+        
 
