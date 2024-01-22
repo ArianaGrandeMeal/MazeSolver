5099ca7d32f742ac959272b40815510693eb070c
ArianaGrandeMeal <trippsellers22@gmail.com>
2024-01-22 16:46:27 -0500

    Final commit, program is fully functional.
    Added solve(), _solve_r(), and _try_cell() methods to Maze class.  Set recursion limit to 10,000, and dded  solve instruction and success/fail print messages to main.py.  Edited test suite to double verify _reset_visited_cell method.

--- maze.py
+++ maze.py
@@ -11,7 +11,7 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
-        if seed is not None:
+        if seed:
             random.seed(seed)
         
         self._create_cells()
@@ -39,7 +39,6 @@ class Maze:
         y1 = (j * self._cell_size_y) + self._y1
         x2 = x1 + self._cell_size_x
         y2 = y1 + self._cell_size_y
-
         self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
         
@@ -48,12 +47,11 @@ class Maze:
         if self._win is None:
             return
         self._win.redraw()
-        time.sleep(0.01)
+        time.sleep(0.05)
 
     def _break_entrance_and_exit(self):
         self._cells[0][0].has_top_wall = False
         self._draw_cell(0,0)
-
         self._cells[-1][-1].has_bottom_wall = False
         self._draw_cell(self._num_cols-1, self._num_rows-1)
 
@@ -67,52 +65,99 @@ class Maze:
 
         while True:
             
-            _to_visit = []
+            to_visit = []
             # determine which cell to visit
             # check left cell 
             if i > 0 and self._cells[l][j].visited == False:
-                _to_visit.append((l, j))
+                to_visit.append((l, j))
             # check above cell
             if j > 0 and self._cells[i][u].visited == False:
-                _to_visit.append((i, u))
+                to_visit.append((i, u))
             # check right cell
             if i < self._num_cols - 1 and self._cells[r][j].visited == False:
-                _to_visit.append((r, j))
+                to_visit.append((r, j))
             # check lower cell
             if j < self._num_rows -1 and self._cells[i][d].visited == False:
-                _to_visit.append((i, d))
+                to_visit.append((i, d))
             
             # draw cell if no options, otherwise choose an adjacent cell
-            if not _to_visit:
+            if not to_visit:
                 self._draw_cell(i, j)
                 return
             
-            _direction = random.randrange(len(_to_visit))
-            _to_index = _to_visit[_direction]
+            # assign cell to visit
+            direction = random.randrange(len(to_visit))
+            to_index = to_visit[direction]
             
             # knock down walls between current and chosen cell
             # chosen cell is right, remove right of current and left of chosen
-            if _to_index[0] == r:
+            if to_index[0] == r:
                 _current.has_right_wall = False
                 self._cells[r][j].has_left_wall = False
             # chosen cell is left, remove left of current and right of chosen
-            if _to_index[0] == l:
+            if to_index[0] == l:
                 _current.has_left_wall = False
                 self._cells[l][j].has_right_wall = False
             # chosen cell is below, remove bottom of current and top of chosen
-            if _to_index[1] == d:
+            if to_index[1] == d:
                 _current.has_bottom_wall = False
                 self._cells[i][d].has_top_wall = False
             # chosen cell is above, remove top of current and bottom of chosen
-            if _to_index[1] == u:
+            if to_index[1] == u:
                 _current.has_top_wall = False
                 self._cells[i][u].has_bottom_wall = False
            
             # recursively visit next cell
-            self._break_walls_r(_to_index[0], _to_index[1])
+            self._break_walls_r(to_index[0], to_index[1])
             
     def _reset_cells_visited(self):
         for col in self._cells:
             for cell in col:
                 cell.visited = False
-            
+
+    # helper for _sole_r() method to improve readability while iteratively checking 
+    # adjacent cells-x,y represent i,j indexes for each-for open path.  
+    # if cell is valid, move is drwan and _solve_r() is called recursively on successive
+    # cells, otherwise, an undo move is drawn
+    def _try_cell(self, current, x, y):
+        current.draw_move(self._cells[x][y])
+        if self._solve_r(x,y) == True:
+            return True
+        current.draw_move(self._cells[x][y], True)    
+    
+    def _solve_r(self, i, j):
+        self._animate()
+        
+        # set and visit current cell
+        current = self._cells[i][j]
+        current.visited = True
+        
+        # if current cell is end cell, maze is solved
+        if current == self._cells[self._num_cols-1][self._num_rows-1]:
+            return True
+        
+        l = i-1
+        r = i+1
+        u = j-1
+        d = j+1
+        directions = [[l, j, 'left'], [i, u, 'top'], [r, j, 'right'], [i, d, 'bottom']]
+        for x, y, dir in directions:
+        # Make sure we are within grid and path is open, then _try_cell() recrusively attempts
+        # to check and draw path through successive cells in same direction
+            if 0 <= x < self._num_cols and 0 <= y < self._num_rows:
+                if dir == 'left' and not current.has_left_wall and not self._cells[x][y].visited:
+                    self._try_cell(current, x, y)
+                elif dir == 'top' and not current.has_top_wall and not self._cells[x][y].visited:
+                    self._try_cell(current, x, y)
+                elif dir == 'right' and not current.has_right_wall and not self._cells[x][y].visited:
+                    self._try_cell(current, x, y)
+                elif dir == 'bottom' and not current.has_bottom_wall and not self._cells[x][y].visited:
+                    self._try_cell(current, x, y)
+        
+        # signifies a wall or dead end in attempted direction, returns false 
+        return False
+                
+    # creates moves for solution using depth-first search.  
+    def solve(self):
+        return self._solve_r(0,0)
+    
--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, time
 from maze import Maze
 
 class Tests(unittest.TestCase):
@@ -15,64 +15,50 @@ class Tests(unittest.TestCase):
             num_rows
             )
         
-    def test_maze_2rows_15cols(self):
-        num_cols = 15
-        num_rows = 2
-        m2 = Maze(20, 20, num_rows, num_cols, 5, 20)
-        self.assertEqual(
-            len(m2._cells),
-            num_cols,
-            )
-        self.assertEqual(
-            len(m2._cells[0]),
-            num_rows
-            )
-        
-    def test_maze_20rows_100cols(self):
-        num_cols = 100
-        num_rows = 20
-        m3 = Maze(0, 0, num_rows, num_cols, 10, 10)
-        self.assertEqual(
-            len(m3._cells),
-            num_cols,
-            )
-        self.assertEqual(
-            len(m3._cells[0]),
-            num_rows
-            )
         
     def test_entrance_exit_break(self):
         num_cols = 10
         num_rows = 10
-        m4 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
-            len(m4._cells), 
+            len(m1._cells), 
             num_cols,
             )
         self.assertEqual(
-            len(m4._cells[0]), 
+            len(m1._cells[0]), 
             num_rows
             )
         
-        if m4._cells[0][0].has_top_wall:
+        if m1._cells[0][0].has_top_wall:
             print("Failed to break entrance")
 
-        if m4._cells[-1][-1].has_bottom_wall:
+        if m1._cells[-1][-1].has_bottom_wall:
             print("Failed to break exit")
         
     def test_reset_visited(self):
         num_cols = 5
         num_rows = 5
-        m5 = Maze(0, 0, num_rows, num_cols, 10, 10)
-        #m5._cells[2][2].visited = True
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         
-        for col in m5._cells:
+        for col in m1._cells:
             for cell in col:
                 self.assertEqual(
                     cell.visited,
                     False,
                 )
-    
+        print("All cells successfully reset.  Rechecking with forced error.")
+
+        # sleep for 2 seconds to allow user to verify correct function, then set one
+        # cell's visited property to True and rerun test to force failure and double 
+        # verify that method works correctly.
+        time.sleep(2)
+        m1._cells[2][2].visited = True
+        for col in m1._cells:
+            for cell in col:
+                self.assertEqual(
+                    cell.visited,
+                    False,
+                )
         
 if __name__ == "__main__":
     unittest.main()
